c5c24f10ac6c227423f356fa7ec680621918b126
nicolas-maskal <88048461+nicolas-maskal@users.noreply.github.com>
2021-07-28 20:54:34 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def connect_points(draw, points, color, height_change, width_change):
     points[0][0] = int(points[0][0] * height_change)
     points[0][1] = int(points[0][1] * width_change)
     for i in range(1, len(points)):
+        # Recalculating the points based on height/width change, dictionary is then saved in a json
         points[i][0] = int(points[i][0] * height_change)
         points[i][1] = int(points[i][1] * width_change)
         draw.line([(points[i - 1][0], points[i - 1][1]),
@@ -35,13 +36,17 @@ def draw_shapes(lesions, image, height_change, width_change):
         draw.text(text_coord, text, color, font=font)
 
 
-def main():
+def handle_input():
     image_path = input("Path to retina image: ")
     json_path = input("Json with annotation: ")
     print("Target dimensions of the image: ", end="")
     try:
         x, y = map(int, input().split())
         new_size = (x, y)
+        if x <= 0 or y <= 0:
+            print("Height and width must be > 0.")
+            print("Aborting script...")
+            return
     except ValueError:
         print("Invalid target dimensions.")
         print("Aborting script...")
@@ -52,9 +57,18 @@ def main():
             data = json.load(f1)
 
         image = Image.open(image_path)
-    except:
+    except (FileNotFoundError, PermissionError):
         print("Error handling files")
         print("Aborting script...")
+        return
+
+    return image, data, new_size
+
+
+def main():
+    try:
+        image, data, new_size = handle_input()
+    except TypeError:
         return
 
     lesions = data['segmentedObjectDict']
